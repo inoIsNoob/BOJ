d64dae67689faf58916f4974442cba84e094bdb5
inoIsNoob <inochoi@g.hongik.ac.kr>
2025-06-06 22:30:29 +0900

    시험 감독 #수학
    i-b<0 인 경우는 주의

---
+++ Bronze/Bronze II/13458.py
@@ -0,0 +1,13 @@
+n = int(input())
+a = list(map(int, input().split()))
+b,c = map(int, input().split())
+
+res = 0
+for i in a:
+    if i-b>0:
+        if (i-b)%c == 0:
+            res += (i-b)//c
+        else:
+            res += (i-b)//c + 1
+
+print(res + n)
