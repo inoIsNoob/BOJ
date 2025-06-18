0f72b8b84de6b3089aaf1a6db543b4d746fb073e
inoIsNoob <inochoi@g.hongik.ac.kr>
2025-06-18 22:55:27 +0900

    Kaučuk #구현
    출력형식만.. 그리고 better version 추가함

--- Bronze/Bronze I/23687.py
+++ Bronze/Bronze I/23687.py
@@ -14,3 +14,21 @@ for _ in range(int(input())):
         secNum = str(sec) + '.' + str(subsec) + '.' + str(subsubsec)
     
     print(secNum, word)
+
+########################################################################################
+
+sec = [0, 0, 0]
+for _ in range(int(input())):
+    command, word = input().split()
+    if command == 'section':
+        sec[0] += 1
+        sec[1] = 0
+        sec[2] = 0
+    elif command == 'subsection':
+        sec[1] += 1
+        sec[2] = 0
+    elif command == 'subsubsection':
+        sec[2] += 1
+    
+    print('.'.join(map(str, 
+    [x for x in sec if x])), word)
