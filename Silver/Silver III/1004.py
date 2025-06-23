44fac5769513a889ab3e7f9aabff0d8b0a6e9b96
inoIsNoob <inochoi@g.hongik.ac.kr>
2025-06-23 22:11:44 +0900

    어린 왕자

---
+++ Silver/Silver III/1004.py
@@ -0,0 +1,17 @@
+import sys
+input = sys.stdin.readline
+
+for _ in range(int(input())):
+    x1, y1, x2, y2 = map(int, input().split())
+    n = int(input())
+    res = 0
+    
+    for _ in range(n):
+        cx, cy, r = map(int, input().split())
+        d1 = ((x1-cx)**2 + (y1-cy)**2)**0.5
+        d2 = ((x2-cx)**2 + (y2-cy)**2)**0.5
+        if d1 < r or d2 < r:
+            if not (d1 < r and d2 < r):
+                res += 1
+    
+    print(res)
